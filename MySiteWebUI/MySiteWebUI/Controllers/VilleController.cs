ea13e84dcb016beb5e34b4a7e461feeb69c2ca14
IbrahimaSall <isall@groupeastek.fr>
2023-02-18 13:04:34 +0100

    First commit

---
+++ MySiteWebUI/MySiteWebUI/Controllers/VilleController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySiteWebUI.Models;
+
+namespace MySiteWebUI.Controllers
+{
+    public class VilleController : Controller
+    {
+        public IActionResult Index()
+        {
+            int a = 10;
+            var ville = new ToutesLesVillesViewModel();
+            ville.Nom_ville = "Labé";
+            ville.Nombre_Quartier = 25;
+
+            return View(ville);
+        }
+    }
+}
---
+++ MySiteWebUI/MySiteWebUI/Models/ToutesLesVillesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Permissions;
+
+namespace MySiteWebUI.Models
+{
+    public class ToutesLesVillesViewModel
+    {
+        public string Nom_ville
+        {
+            get; set;
+        }
+
+        public int Nombre_Quartier { get; set; }
+    }
+}
