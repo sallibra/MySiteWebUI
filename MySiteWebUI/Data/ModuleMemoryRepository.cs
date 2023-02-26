a02eac54681e598aafb65c1a7a55922620eafbcb
IbrahimaSall <isall@groupeastek.fr>
2023-02-26 17:59:19 +0100

    Add data class

---
+++ MySiteWebUI/Data/ModuleMemoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ModuleMemoryRepository
+    {
+        private List<LesModules> _modules = new List<LesModules>();
+
+        public ModuleMemoryRepository() 
+        { 
+            _modules.Add(new LesModules { Id = 1, Nom = "Le C Sharp << C# >> ", NomUrl = "asp-net-core", Description= "Decouvrir le developpement C# .NET core"});
+            _modules.Add(new LesModules { Id = 2, Nom = "Le SQL", NomUrl = "bdd-sql", Description = "Decouvrir le sql server" });
+
+        }
+        public List<LesModules> GetAllModules() 
+        {
+            return _modules;
+        }
+    }
+}
---
+++ MySiteWebUI/MySiteWebUI/Controllers/ModuleController.cs
@@ -0,0 +1,27 @@
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
+using MySiteWebUI.Models;
+
+namespace MySiteWebUI.Controllers
+{
+    public class ModuleController : Controller
+    {
+        public IActionResult Index()
+        {
+            int a = 10;
+            var ville = new ToutesLesVillesViewModel();
+            ville.Nom_ville = "Labé";
+            ville.Nombre_Quartier = 25;
+
+            return View(ville);
+        }
+
+        public IActionResult TousLesModules() 
+        {
+            ModuleMemoryRepository repository = new ModuleMemoryRepository();
+            List<LesModules> listModule = repository.GetAllModules();
+            return View(listModule);
+        }
+
+    }
+}
